59f9d3c884944a3d50811406aef2e416f42c3c54
how123480 <how123480@gmail.com>
2020-10-16 19:50:58 +0800

    google function example

--- vote/app.py
+++ vote/app.py
@@ -5,10 +5,15 @@ import socket
 import random
 import json
 
+import requests
+import base64
+import urllib
+import subprocess
+
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
-
+cloud_func_url = "https://us-central1-avian-force-199001.cloudfunctions.net/randomCAT-DOG"
 app = Flask(__name__)
 
 def get_redis():
@@ -23,11 +28,14 @@ def hello():
         voter_id = hex(random.getrandbits(64))[2:-1]
 
     vote = None
+    ad_img = None
 
     if request.method == 'POST':
         redis = get_redis()
         vote = request.form['vote']
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        img = requests.post(cloud_func_url, data={"vote":vote})
+        ad_img = base64.b64encode(img.content).decode('ascii')
         redis.rpush('votes', data)
 
     resp = make_response(render_template(
@@ -36,6 +44,7 @@ def hello():
         option_b=option_b,
         hostname=hostname,
         vote=vote,
+        ad=ad_img
     ))
     resp.set_cookie('voter_id', voter_id)
     return resp
